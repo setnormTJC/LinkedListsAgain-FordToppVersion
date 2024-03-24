d88eedead334179c9e85e2e6c66e47c3ee0fe125
snorm <snorm@MSI>
2024-03-24 17:05:09 -0500

    Add project files.

---
+++ LinkedListsAgain-FordToppVersion/FordToppLinkedList.h
@@ -0,0 +1,30 @@
+#pragma once
+
+//template<typename T> 
+//class dnode
+//{
+//public:
+//	T nodeValue; 
+//	dnode<T>* next; 
+//	dnode<T>* prev;
+//
+//
+//	dnode()
+//		:next(nullptr), prev(nullptr), nodeValue(T())
+//	{};
+//
+//	dnode(const T& value)
+//		:nodeValue(value)
+//	{
+//
+//	}
+//
+//	void insert(const T& item)
+//	{
+//		dnode<T>* newNode, * prevNode; 
+//
+//		newNode = new dNode<T>(item); 
+//
+//
+//	}
+//};
